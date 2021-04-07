515cdb3f22635d97ba5d5eb52fdf060c4bc08bde
Jose Antonio Insua <insuafer@adobe.com>
2021-04-07 03:03:03 +0200

    Add new validate command

---
+++ cmd/validate.go
@@ -0,0 +1,35 @@
+// Copyright 2021 Adobe. All rights reserved.
+// This file is licensed to you under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may obtain a copy
+// of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under
+// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+// OF ANY KIND, either express or implied. See the License for the specific language
+// governing permissions and limitations under the License.
+
+package cmd
+
+import (
+	"github.com/adobe/imscli/ims"
+	"github.com/spf13/cobra"
+)
+
+func validateCmd(imsConfig *ims.Config) *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:   "validate",
+		Short: "Validates a token using the IMS API.",
+		Long: `Verifies if the specified IMS token is valid using the IMS API.
+
+This command has no effect by itself, the token type must be specified as a subcommand.
+`,
+	}
+	cmd.AddCommand(
+		validateAccessTokenCmd(imsConfig),
+		validateRefreshTokenCmd(imsConfig),
+		validateDeviceTokenCmd(imsConfig),
+		validateAuthzCodeCmd(imsConfig),
+	)
+	return cmd
+}
---
+++ cmd/validate_authorization_code.go
@@ -0,0 +1,43 @@
+// Copyright 2021 Adobe. All rights reserved.
+// This file is licensed to you under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may obtain a copy
+// of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under
+// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+// OF ANY KIND, either express or implied. See the License for the specific language
+// governing permissions and limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/adobe/imscli/ims"
+	"github.com/spf13/cobra"
+)
+
+func validateAuthzCodeCmd(imsConfig *ims.Config) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "authorizationCode",
+		Aliases: []string{"authz"},
+		Short:   "Validate an authorization code.",
+		Long:    "Validate an authorization code.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SilenceUsage = true
+			cmd.SilenceErrors = true
+
+			resp, err := imsConfig.AuthorizeJWTExchange()
+			if err != nil {
+				return fmt.Errorf("error validating the authorization code: %v", err)
+			}
+			fmt.Println(resp)
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&imsConfig.AuthorizationCode, "authorizationCode", "t", "", "Authorization code.")
+	cmd.Flags().StringVarP(&imsConfig.ClientID, "clientID", "c", "", "IMS Client ID.")
+
+	return cmd
+}
