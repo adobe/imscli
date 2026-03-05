3ef9ed2b65539eaf72e7235207acb397e07128cf
Halil Cankilic <141903706+Eiermitsucuk@users.noreply.github.com>
2026-03-05 10:05:49 +0100

    feat: obo-exchange (#90)
    Co-authored-by: Jose Antonio Insua <insuafer@adobe.com>

--- ims/config.go
+++ ims/config.go
@@ -53,7 +53,7 @@ type Config struct {
 	AuthSrc               string
 	DecodeFulfillableData bool
 	ClientName            string
-	RedirectURIs          []string
+	RedirectURIs []string
 }
 
 // TokenInfo holds the response data from token-related IMS API calls.
